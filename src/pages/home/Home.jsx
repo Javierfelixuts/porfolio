e02ec9d126d4929e2c9d24781712adff0091963a
JavierAFT <javierantoniofelixtorres@gmail.com>
2022-03-08 09:49:32 -0700

    Quitar variables sin uso

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import Bienvenida from '../../components/bienvenida/Bienvenida'
-import Mensaje from '../../components/mensaje/Mensaje'
 import Tecnologias from '../../components/tecnologias/Tecnologias'
 
 const Home = () => {
   return (
     <>
         <Bienvenida />
-        {/* <Mensaje
-          mensaje={" Estos son los lenguajes que he aprendido a lo largo de mi carrera para ser Programador Web ademas algunas herramientas que me ha tocado utilizar."}
-        /> */}
+        
         <Tecnologias />
     </>
   )
