fbc8dc474fead6b626838d1a057857530b216977
JavierAFT <javierantoniofelixtorres@gmail.com>
2022-03-08 12:35:59 -0700

    center scroll animation

--- src/components/scrollDown/ScrollDown.jsx
+++ src/components/scrollDown/ScrollDown.jsx
@@ -3,12 +3,12 @@ import './scrollDown.css'
 
 const ScrollDown = () => {
   return (
-    <div class="container">
-        <div className="container__chevron">
-            <div class="chevron"></div>
-            <div class="chevron"></div>
-            <div class="chevron"></div>
-            <span class="text">Seguir Bajando</span>
+    <div className="container">
+        <div classNameName="container__chevron">
+            <div className="chevron"></div>
+            <div className="chevron"></div>
+            <div className="chevron"></div>
+            <span className="text">Seguir Bajando</span>
         </div>
     </div>
   )
