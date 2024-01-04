07f144390eb7d68578bafb3f94eabd1130681617
unknown <felixtorresjavierantonio@gmail.com>
2024-01-03 22:14:38 -0700

    [UPDATE] HEADER

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -42,6 +42,9 @@ const Header = () => {
             <NavLink to="certificates" onClick={handleClickMenu}
               className={({ isActive }) => isActive ? 'nav-link' : ''}
             >CERTIFICADOS</NavLink>
+            <NavLink to="projectos" onClick={handleClickMenu}
+              className={({ isActive }) => isActive ? 'nav-link' : ''}
+            >PROYECTOS</NavLink>
             <NavLink to="contacto" onClick={handleClickMenu}
               className={({ isActive }) => isActive ? 'nav-link' : ''}
             >CONTACTO</NavLink>
